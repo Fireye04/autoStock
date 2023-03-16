6f36a0a81e92f14c0f08147dbbfe57ad5e2e7624
Kai Koehler <codekai16@gmail.com>
2023-03-16 14:30:40 -0700

    init

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import pyautogui
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screenWidth, screenHeight = pyautogui.size()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+i = 0
+while True:
+    if i % 5000000 == 0:
+        currentMouseX, currentMouseY = pyautogui.position()
+        print(f"{currentMouseX}, {currentMouseY}")
+    i+=1
