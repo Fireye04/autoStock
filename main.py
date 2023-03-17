5dcba46bf5abfdb2fa23e2f29e8a68e3454b26ba
Kai Koehler <codekai16@gmail.com>
2023-03-16 17:25:35 -0700

    e

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import pyautogui
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Firefox()
+browser.get('https://www.marketwatch.com/games/epic-gaymers-class')
+
+
+elem = browser.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
 
 screenWidth, screenHeight = pyautogui.size()
 
-i = 0
-while True:
-    if i % 5000000 == 0:
-        currentMouseX, currentMouseY = pyautogui.position()
-        print(f"{currentMouseX}, {currentMouseY}")
-    i+=1
+# i = 0
+# while True:
+#     if i % 5000000 == 0:
+#         currentMouseX, currentMouseY = pyautogui.position()
+#         print(f"{currentMouseX}, {currentMouseY}")
+#     i+=1
