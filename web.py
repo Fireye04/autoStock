087e2dd6d2b2b8f2b23420b5f60fed1bf4c4c92e
Kai Koehler <codekai16@gmail.com>
2023-03-17 21:54:59 -0700

    trade function

--- web.py
+++ web.py
@@ -7,46 +7,12 @@ import time
 
 browser = webdriver.Firefox()
 
-
 def delay(duration):
     for i in range(duration):
         print(i + 1)
         time.sleep(1)
 
-    print("\n")
-
-
-def login(email: str, password: str):
-    browser.get('https://www.marketwatch.com/games/epic-gaymers-class')
-
-    # wait for page loading
-    delay(5)
-
-    elem = browser.find_element(By.CLASS_NAME, 'close-btn')  # Find the search box
-    # elem.send_keys(Keys.RETURN)
-    elem.click()
-
-    loginfo = browser.find_element(By.CLASS_NAME, "profile.logged-out")
-    loginfo.click()
-
-    loginfo = browser.find_element(By.LINK_TEXT, "Log In")
-    loginfo.click()
-
-    delay(5)
-
-    typeBox = browser.find_element(By.ID, "username")
-    typeBox.send_keys(email)
-
-    nextButton = browser.find_element(By.CLASS_NAME, "solid-button.continue-submit.new-design")
-    nextButton.click()
-
-    delay(3)
-
-    typeBox = browser.find_element(By.ID, "password-login-password")
-    typeBox.send_keys(password)
-
-    nextButton = browser.find_element(By.CLASS_NAME, "solid-button.new-design.basic-login-submit")
-    nextButton.click()
+    print(" ")
 
 # uncomment this to change the acting email and password
 #
@@ -58,4 +24,59 @@ def login(email: str, password: str):
 with open("info.p", "rb") as data:
     info = pickle.load(data)
 
-login(info[0], info[1])
+email = info[0]
+password = info[1]
+
+
+def login() -> bool:
+    try:
+        browser.get('https://www.marketwatch.com/games/epic-gaymers-class')
+
+        # wait for page loading
+        delay(5)
+
+        elem = browser.find_element(By.CLASS_NAME, 'close-btn')  # Find the search box
+        # elem.send_keys(Keys.RETURN)
+        elem.click()
+
+        loginfo = browser.find_element(By.CLASS_NAME, "profile.logged-out")
+        loginfo.click()
+
+        loginfo = browser.find_element(By.LINK_TEXT, "Log In")
+        loginfo.click()
+
+        delay(5)
+
+        typeBox = browser.find_element(By.ID, "username")
+        typeBox.send_keys(email)
+
+        nextButton = browser.find_element(By.CLASS_NAME, "solid-button.continue-submit.new-design")
+        nextButton.click()
+
+        delay(3)
+
+        typeBox = browser.find_element(By.ID, "password-login-password")
+        typeBox.send_keys(password)
+
+        nextButton = browser.find_element(By.CLASS_NAME, "solid-button.new-design.basic-login-submit")
+        nextButton.click()
+
+        return True
+    except Exception as e:
+        print(f"FAILURE: {e}")
+        browser.quit()
+        return False
+
+
+def trade():
+    if not login():
+        return
+
+
+
+    inp = input("Quit? ")
+    if inp == "y":
+        browser.quit()
+
+
+trade()
