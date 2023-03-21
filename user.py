372354067aec934a8d026d5bc964d04aefc280e9
Kai Koehler <codekai16@gmail.com>
2023-03-21 15:28:09 -0700

    get stats

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 import pickle
-
+import yfinance as yf
 
 class User:
     def __init__(self, userName: str, currentFunds: int, currentStockList: [[str, int], ...]):
@@ -7,21 +7,25 @@ class User:
 
         self.funds = currentFunds
 
-        #paired list [stock: str, money in stock: int]
+        # paired list [stock: str, money in stock: int]
         self.stocks = currentStockList
 
-        #just the strs
+        # just the strs
         self.plainStocks = []
 
-        for i in range(len(self.stocks)):
-            self.plainStocks.append(self.stocks[i][0])
+        self.plainList()
 
         # calculates funds in stock
         self.fundsInStock = 0
 
-        # iterates over stockList and adds all stock values
-        for i in range(len(self.stocks)):
-            self.fundsInStock += self.stocks[i][1]
+        self.iterateFunds()
+
+    def buyStock(self, ticker: str, numberOfShares: int):
+        stok = yf.Ticker(ticker).fast_info["lastPrice"]
+
+
+    def sellStock(self):
+        pass
 
     def setFunds(self, amount: int, increment: bool):
         if increment:
@@ -29,19 +33,42 @@ class User:
         else:
             self.funds = amount
 
+    def setStocks(self, newStocks: [[str, int], ...]):
+        self.stocks = newStocks
+
+    def appendStocks(self, newStock: [str, int]):
+        self.stocks.append(newStock)
+
+    def removeStocks(self, remStock: str):
+        for stock in self.stocks:
+            if stock[0] == remStock:
+                self.stocks.remove(stock)
+
+    def plainList(self):
+        self.plainStocks = []
+
+        for i in range(len(self.stocks)):
+            self.plainStocks.append(self.stocks[i][0])
+
+    def iterateFunds(self):
+        self.fundsInStock = 0
+
+        # iterates over stockList and adds all stock values
+        for i in range(len(self.stocks)):
+            self.fundsInStock += self.stocks[i][1]
+
     def __str__(self):
         return f"{self.name}'s Stock Profile- \n~~~\nNet Worth- ${self.funds + self.fundsInStock} \n" \
                f"Funds- ${self.funds} \nMoney In Stocks- ${self.fundsInStock} \nStocks- {self.plainStocks} \n" \
                f"Paired Stocks- {self.stocks}"
 
-
 # uncomment this to change the acting user
 #
-user = User("Name", 100_000, [[]])
-with open(user.name + ".p", "wb") as data:
-    pickle.dump(user, data)
+# user = User("Name", 100_000, [[]])
+# with open(user.name + ".p", "wb") as data:
+#     pickle.dump(user, data)
 
-with open("Name.p", "rb") as data:
-    user = pickle.load(data)
-
-print(user)
+# with open("Name.p", "rb") as data:
+#     user = pickle.load(data)
+#
+# print(user)
--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+# haha poopy haha so funny amogus im so funny hahahhaahhaahha
 import pickle
 import time
 
@@ -30,13 +30,12 @@ email = info[0]
 password = info[1]
 
 
-def login() -> bool:
+def login(website='https://www.marketwatch.com/games/epic-gaymers-class') -> bool:
     try:
-        browser.get('https://www.marketwatch.com/games/epic-gaymers-class')
+        browser.get(website)
 
         # wait for page loading
         delay(5)
-
         elem = browser.find_element(By.CLASS_NAME, 'close-btn')  # Find the search box
         # elem.send_keys(Keys.RETURN)
         elem.click()
@@ -47,7 +46,7 @@ def login() -> bool:
         loginfo = browser.find_element(By.LINK_TEXT, "Log In")
         loginfo.click()
 
-        delay(3)
+        delay(1)
 
         typeBox = browser.find_element(By.ID, "username")
         typeBox.send_keys(email)
@@ -55,7 +54,7 @@ def login() -> bool:
         nextButton = browser.find_element(By.CLASS_NAME, "solid-button.continue-submit.new-design")
         nextButton.click()
 
-        delay(3)
+        delay(1)
 
         typeBox = browser.find_element(By.ID, "password-login-password")
         typeBox.send_keys(password)
@@ -79,4 +78,36 @@ def trade():
         browser.quit()
 
 
-trade()
+def updateStats(site='https://www.marketwatch.com/games/epic-gaymers-class'):
+    if not login(site):
+        return
+
+    delay(3)
+
+    browser.get(site + "/portfolio")
+
+    delay(3)
+
+    lisst = browser.find_element(By.CLASS_NAME, "table.table--secondary.holdings")
+    body = lisst.find_element_by_tag_name("tbody")
+    holdings = body.find_elements_by_tag_name("tr")
+
+    stonks = []
+    for hold in holdings:
+        stonks.append(hold.text)
+
+    stats = []
+
+    for stonk in stonks:
+        s = stonk.split("\n")
+        stats.append(s)
+
+    # Ticker, Shares owned, Type (buy/short), current share price, $change, %change, $ value, total value $change/%change
+    print(stats)
+
+    inp = input("Quit? ")
+    if inp == "y":
+        browser.quit()
+
+
+updateStats("https://www.marketwatch.com/games/tgseblock")
